e837e2ef14b3eaf154cee2018ad6912df47fec92
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-01-29 22:58:51 +0100

    added plugin for forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1600px',
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 }
