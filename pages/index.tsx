044fd3c2409c78d92c8366eb047e1fb6b0bc5698
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-08 23:48:36 +0100

    wip, empty shell

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
+import Header from "../components/Header"
 // import { Camera } from '@heroicons/react/outline';
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
       <Head>
         <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>This is the INSTAGRAM 2.0 build</h1>
+      
       <Header />
+
       {/* Feed */}
+      <Feed />
+
       {/* Modal */}
     </div>
   )
