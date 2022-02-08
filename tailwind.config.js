3a15b0119ad03e5e141883cf42b42a840138d729
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-08 23:47:59 +0100

    added plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,9 @@ module.exports = {
       }
     },
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("tailwind-scrollbar"),
+    require("tailwind-scrollbar-hide")
+],
 }
