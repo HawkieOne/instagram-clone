d5efcee86b0d9e9496d48847a285917e36f8cf14
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-13 19:44:19 +0100

    partly only visible when logged in

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import faker from "@faker-js/faker";
 import Story from './Story';
+import { useSession } from 'next-auth/react';
 
 export default function Stories() {
 
     const [suggestions, setSuggestions] = useState<Array<any>>([]);
+    const { data: session } = useSession();
 
     useEffect(() => {
         const suggestions : object[] = [...Array(20)].map((_, i) => ({ 
@@ -25,6 +27,14 @@ export default function Stories() {
     return (
         <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm
                         overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+
+            {session && (
+                <Story  
+                    img={session?.user?.image}
+                    username={session?.user?.username!}
+                />
+            )}
+
             {suggestions.map((profile) => (
                 <Story 
                     key={profile.id}  
