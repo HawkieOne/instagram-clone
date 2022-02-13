af91d006489f2ae191426553a1fb0ac74964b807
Håkan Lindahl <59147469+HawkieOne@users.noreply.github.com>
2022-02-13 19:44:12 +0100

    now no longer uses hard coded data

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,37 +1,19 @@
 import faker from '@faker-js/faker';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
 import Post from './Post';
 import Stories from './Stories';
-
-const posts = [
-    {
-        id: '123',
-        username: 'hawkie',
-        userImg: 'https:///links.papareact.com/3ke',
-        img: 'https:///links.papareact.com/3ke',
-        caption: 'This is dope!',
-    },
-    {
-        id: '123',
-        username: 'hawkie',
-        userImg: 'https:///links.papareact.com/3ke',
-        img: 'https:///links.papareact.com/3ke',
-        caption: 'This is dope!',
-    },
-];
 
 export default function Posts() {
 
     const [posts, setPosts] = useState<Array<any>>([]);
 
-    useEffect(() => {
-        const posts : object[] = [...Array(20)].map((_, i) => ({ 
-            ...faker.helpers.contextualCard(),
-            id: i,
-        }));
-        console.log(posts);
-        setPosts(posts);
-    }, [])
+    useEffect(() => 
+        onSnapshot(query(collection(db, 'posts'),  orderBy('timestamp', 'desc')), 
+        (snapshot) => {
+            setPosts(snapshot.docs);
+    }), [db])
 
   return (
       <div>
@@ -40,10 +22,10 @@ export default function Posts() {
                   <Post 
                     key={post.id}
                     id={post.id}
-                    username={post.username}
-                    userImg={post.avatar}
-                    img={post.avatar}
-                    caption={post.company.catchPhrase}
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
                   />
               )))
           }
